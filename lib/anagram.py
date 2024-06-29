3e1ce13c6516fe0e6da40aad873dc72bf7bbdce8
Joseph-Chege <mjchege23@gmail.com>
2024-06-29 09:53:01 +0300

    Created an anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word=None):
+        self._word = None
+        if word is not None:
+            self.word = word
+
+    def match(self, list):
+        sorted_word = sorted(self.word)
+        return [word for word in list if sorted(word) == sorted_word]
+               
+    
+       
+           
+
+
+
+
+
+
+        
+
+
